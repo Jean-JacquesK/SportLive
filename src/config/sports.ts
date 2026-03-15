336ff64df1abaab7de24f1f417d86c5edb53f553
Jean-Jacques Kechichian <jj.kechichian@gmail.com>
2026-03-15 21:36:32 +0100

    Configuration des types de sports

---
+++ src/config/sports.ts
@@ -0,0 +1,39 @@
+import { Sport } from "../types/sports";
+
+export const SPORTS : Sport[] = [
+  {
+    id: "nba",
+    label: "NBA",
+    emoji: "🏀",
+    sport: "basketball",
+    league: "nba",
+  },
+  {
+    id: "nfl",
+    label: "NFL",
+    emoji: "🏈",
+    sport: "football",
+    league: "nfl",
+  },
+  {
+    id: "soccer",
+    label: "Football",
+    emoji: "⚽",
+    sport: "soccer",
+    league: "esp.1",
+  },
+  {
+    id: "nhl",
+    label: "NHL",
+    emoji: "🏒",
+    sport: "hockey",
+    league: "nhl",
+  },
+  {
+    id: "mlb",
+    label: "MLB",
+    emoji: "⚾",
+    sport: "baseball",
+    league: "mlb",
+  },
+]
